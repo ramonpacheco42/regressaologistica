3722d6d724c873a92bd82d842c7808e6365dc7f1
ramonpacheco42 <74688664+ramonpacheco42@users.noreply.github.com>
2023-06-04 23:05:42 -0300

    Primeiros passos curso

---
+++ modelo_logistico_binario.py
@@ -0,0 +1,10 @@
+# %%
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy import stats
+from statsmodels.iolib.summary2 import summary_col
+import plotly.graph_objs as go
+import statsmodels.formula.api as smf
+# %%
