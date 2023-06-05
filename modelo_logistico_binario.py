e01a3c2c1a1afdbcc5afa982940ae4acccd9ce31
ramonpacheco42 <74688664+ramonpacheco42@users.noreply.github.com>
2023-06-05 00:28:07 -0300

    Aula 01 bloco 1 e 2

--- modelo_logistico_binario.py
+++ modelo_logistico_binario.py
@@ -2,9 +2,85 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 import statsmodels.api as sm
 from scipy import stats
 from statsmodels.iolib.summary2 import summary_col
 import plotly.graph_objs as go
 import statsmodels.formula.api as smf
+
+import warnings
+warnings.filterwarnings('ignore')
+# %%
+#############################################################################
+#               REGRESSÃO LOGÍSTICA BINÁRIA - PARTE CONCEITUAL              #
+#############################################################################
+
+#Estabelecendo uma função para a probabilidade de ocorrência de um evento
+
+from math import exp
+
+#Estabelecendo uma função para a probabilidade de ocorrência de um evento
+def prob(z):
+    return 1 / (1 + exp(-z))
+# %%
+# Plotando a curva sigmóide teórica de ocorrência de um evento para um
+#range do logito z entre -5 e +5
+
+logitos = []
+probs = []
+
+for i in np.arange(-5,6):
+    logitos.append(i)
+    probs.append(prob(i))
+
+df = pd.DataFrame({'logito':logitos,'probs':probs})
+
+# Sigmoide
+plt.figure(figsize=(10,10))
+plt.plot(df.logito, df.probs, color="#440154FF")
+plt.scatter(df.logito, df.probs, color = "#440154FF")
+plt.axhline(y = df.probs.mean(), color = '#bdc3c7', linestyle = ':')
+plt.xlabel("Logito Z", fontsize=20)
+plt.ylabel("Probabilidade", fontsize=20)
+plt.show()
+# %%
+#############################################################################
+#                      REGRESSÃO LOGÍSTICA BINÁRIA                          #                  
+#               EXEMPLO 01 - CARREGAMENTO DA BASE DE DADOS                  #
+#############################################################################
+
+df_atrasado = pd.read_csv('data/atrasado.csv',delimiter=',')
+df_atrasado
+# %%
+#Características das variáveis do dataset
+df_atrasado.info()
+
+#Estatísticas univariadas
+df_atrasado.describe()
+
+# %%
+# Tabela de frequências absolutas da variável 'atrasado'
+
+df_atrasado['atrasado'].value_counts() 
+
+# %%
+# Estimação de um modelo logístico binário
+
+modelo_atrasos = smf.glm(formula='atrasado ~ dist + sem', data=df_atrasado,
+                         family=sm.families.Binomial()).fit()
+
+#Parâmetros do modelo
+modelo_atrasos.summary()
+# %%
+# Outro modo mais completo de apresentar os outputs do modelo,
+#pela função 'summary_col'
+
+summary_col([modelo_atrasos],
+            model_names=["MODELO"],
+            stars=True,
+            info_dict = {
+                'N':lambda x: "{0:d}".format(int(x.nobs)),
+                'Log-lik':lambda x: "{:.2f}".format(x.llf)
+        })
 # %%
