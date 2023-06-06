08821a235a335493607451c914b2432918373880
ramonpacheco42 <74688664+ramonpacheco42@users.noreply.github.com>
2023-06-06 00:48:50 -0300

    Aula 01 finalizada

--- modelo_logistico_binario.py
+++ modelo_logistico_binario.py
@@ -84,3 +84,156 @@ summary_col([modelo_atrasos],
                 'Log-lik':lambda x: "{:.2f}".format(x.llf)
         })
 # %%
+#Fazendo predições para o 'modelo_atrasos'.
+#Exemplo: qual a probabilidade média de se chegar atrasado quando o
+#trajeto tem 7 km e passa-se por 10 semáforos no percurso?
+
+modelo_atrasos.predict(pd.DataFrame({'dist':[7], 'sem':[10]}))
+# %%
+# Construção de uma matriz de confusão
+
+# Adicionando os valores previstos de probabilidade na base de dados
+df_atrasado['phat'] = modelo_atrasos.predict()
+
+#Visualizando a base de dados com a variável 'phat'
+df_atrasado
+# %%
+# Construção de função para a definição da matriz de confusão
+
+from sklearn.metrics import confusion_matrix, accuracy_score,\
+    ConfusionMatrixDisplay, recall_score
+
+def matriz_confusao(observado,predicts,cutoff):
+    
+    values = predicts.values
+    
+    predicao_binaria = []
+    
+    for item in values:
+        if item >= cutoff:
+            predicao_binaria.append(1)
+        else:
+            predicao_binaria.append(0)
+    
+    cm = confusion_matrix(observado, predicao_binaria)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+    
+    sensitividade = recall_score(observado, predicao_binaria, pos_label=1)
+    especificidadee = recall_score(observado, predicao_binaria, pos_label=0)
+    acuracia = accuracy_score(observado, predicao_binaria)
+    #Visualizando os principais indicadores desta matriz de confusão
+    indicadores = pd.DataFrame({'Sensitividade':[sensitividade],
+                                'Especificidade':[especificidadee],
+                                'Acurácia':[acuracia]})
+    return indicadores
+# %%
+# Matrizes de confusão propriamente ditas
+
+#Matriz de confusão para cutoff = 0.5
+matriz_confusao(observado=df_atrasado['atrasado'],
+                predicts=df_atrasado['phat'], 
+                cutoff=0.5)
+
+#Matriz de confusão para cutoff = 0.3
+matriz_confusao(observado=df_atrasado['atrasado'],
+                predicts=df_atrasado['phat'], 
+                cutoff=0.3)
+
+#Matriz de confusão para cutoff = 0.7
+matriz_confusao(observado=df_atrasado['atrasado'],
+                predicts=df_atrasado['phat'], 
+                cutoff=0.7)
+# %%
+# Igualando critérios de especificidade e de sensitividade
+
+#Tentaremos estabelecer um critério que iguale a probabilidade de
+#acerto daqueles que chegarão atrasados (sensitividade) e a probabilidade de
+#acerto daqueles que não chegarão atrasados (especificidade).
+
+#ATENÇÃO: o que será feito a seguir possui fins didáticos, apenas. DE NENHUMA
+#FORMA o procedimento garante a maximização da acurácia do modelo!
+
+#Criação da função 'espec_sens' para a construção de um dataset com diferentes
+#valores de cutoff, sensitividade e especificidade:
+
+def espec_sens(observado,predicts):
+    
+    # adicionar objeto com os valores dos predicts
+    values = predicts.values
+    
+    # range dos cutoffs a serem analisados em steps de 0.01
+    cutoffs = np.arange(0,1.01,0.01)
+    
+    # Listas que receberão os resultados de especificidade e sensitividade
+    lista_sensitividade = []
+    lista_especificidade = []
+    
+    for cutoff in cutoffs:
+        
+        predicao_binaria = []
+        
+        # Definindo resultado binário de acordo com o predict
+        for item in values:
+            if item >= cutoff:
+                predicao_binaria.append(1)
+            else:
+                predicao_binaria.append(0)
+                
+        # Cálculo da sensitividade e especificidade no cutoff
+        sensitividade = recall_score(observado, predicao_binaria, pos_label=1)
+        especificidadee = recall_score(observado, predicao_binaria, pos_label=0)
+        
+        # Adicionar valores nas listas
+        lista_sensitividade.append(sensitividade)
+        lista_especificidade.append(especificidadee)
+        
+    # Criar dataframe com os resultados nos seus respectivos cutoffs
+    resultado = pd.DataFrame({'cutoffs':cutoffs,'sensitividade':lista_sensitividade,'especificidade':lista_especificidade})
+    return resultado
+# %%
+# Até o momento, foram extraídos 3 vetores: 'sensitividade',
+#'especificidade' e 'cutoffs'. Assim, criamos um dataframe que contém
+#os vetores mencionados
+
+dados_plotagem = espec_sens(observado = df_atrasado['atrasado'],
+                            predicts = df_atrasado['phat'])
+dados_plotagem
+# %%
+# Visualizando o novo dataframe 'dados_plotagem' e plotando os dados
+#em um gráfico que mostra a variação da especificidade e da sensitividade
+#em função do cutoff
+
+plt.figure(figsize=(10,10))
+plt.plot(dados_plotagem.cutoffs,dados_plotagem.sensitividade, '-o',
+         color="#440154FF")
+plt.plot(dados_plotagem.cutoffs,dados_plotagem.especificidade, '-o',
+         color="#2ecc71")
+plt.legend(['Sensitividade', 'Especificidade'], fontsize=17)
+plt.xlabel("Cuttoff", fontsize=14)
+plt.ylabel("Sensitividade / Especificidade", fontsize=14)
+plt.show()
+# %%
+# Construção da curva ROC
+
+from sklearn.metrics import roc_curve, auc
+
+#Função 'roc_curve' do pacote 'metrics' do sklearn
+
+fpr, tpr, thresholds =roc_curve(df_atrasado['atrasado'],df_atrasado['phat'])
+roc_auc = auc(fpr, tpr)
+
+#Cálculo do coeficiente de GINI
+gini = (roc_auc - 0.5)/(0.5)
+
+#Plotando a curva ROC
+plt.figure(figsize=(10,10))
+plt.plot(fpr,tpr, '-o', color="#9b59b6")
+plt.plot(fpr,fpr, color='gray')
+plt.title("Área abaixo da curva: %f" % roc_auc +
+          " | Coeficiente de GINI: %f" % gini, fontsize=17)
+plt.xlabel("Especificidade", fontsize=14)
+plt.ylabel("Sensitividade", fontsize=14)
+plt.show()
+# %%
