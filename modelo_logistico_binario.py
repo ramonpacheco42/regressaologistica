f9725bd92b100f5ce43510d9937b5e423206d65c
ramonpacheco42 <74688664+ramonpacheco42@users.noreply.github.com>
2023-06-10 19:03:25 -0300

    Aula Final

--- modelo_logistico_binario.py
+++ modelo_logistico_binario.py
@@ -244,10 +244,10 @@ plt.show()
 
 df_atrasado_multinomial = pd.read_csv('data/atrasado_multinomial.csv',delimiter=',')
 df_atrasado_multinomial
-
+# %%
 #Características das variáveis do dataset
 df_atrasado_multinomial.info()
-
+# %%
 #Estatísticas univariadas
 df_atrasado_multinomial.describe()
 # %%
@@ -255,7 +255,7 @@ df_atrasado_multinomial.describe()
 
 #Tabela de frequências absolutas da variável 'atrasado' com labels
 df_atrasado_multinomial['atrasado'].value_counts(sort=False)
-
+# %%
 #Criando uma variável 'atrasado2' a partir da variável 'atrasado',
 #com labels iguais a 0, 1 e 2 e com tipo 'int' (poderia também ser do tipo
 #'float'), a fim de que seja possível estimar o modelo por meio
@@ -272,5 +272,308 @@ df_atrasado_multinomial.loc[df_atrasado_multinomial['atrasado']==
 
 df_atrasado_multinomial['atrasado2'] =\
     df_atrasado_multinomial['atrasado2'].astype('int64') 
-
+df_atrasado_multinomial
 # %%
+# Estimação do modelo logístico multinomial
+
+from statsmodels.discrete.discrete_model import MNLogit
+
+x = df_atrasado_multinomial.drop(columns=['estudante','atrasado','atrasado2'])
+y = df_atrasado_multinomial['atrasado2']
+
+#Esse pacote precisa que a constante seja definida pelo usuário
+X = sm.add_constant(x)
+
+#Estimação do modelo - função 'MNLogit' do pacote
+#'statsmodels.discrete.discrete_model'
+modelo_atrasado = MNLogit(endog=y, exog=X).fit()
+
+#Parâmetros do modelo
+modelo_atrasado.summary()
+# %%
+# Vamos definir uma função 'Qui2' para se extrair a estatística geral
+# do modelo
+
+def Qui2(modelo_multinomial):
+    maximo = modelo_multinomial.llf
+    minimo = modelo_multinomial.llnull
+    qui2 = -2*(minimo - maximo)
+    pvalue = stats.distributions.chi2.sf(qui2,1)
+    df = pd.DataFrame({'Qui quadrado':[qui2],
+                       'pvalue':[pvalue]})
+    return df
+# %%
+# Estatística geral do 'modelo_atrasado'
+
+Qui2(modelo_atrasado)
+# %%
+# Fazendo predições para o 'modelo_atrasado'
+
+# Exemplo: qual a probabilidade média de atraso para cada categoria da
+#variável dependente, se o indivíduo tiver que percorrer 22km e passar
+#por 12 semáforos?
+
+#No nosso exemplo, tempos que:
+# 0: não chegou atrasado
+# 1: chegou atrasado primeira aula
+# 2: chegou atrasado segunda aula
+
+resultado = modelo_atrasado.predict(pd.DataFrame({'const':[1],
+                                                   'dist':[22],
+                                                   'sem':[12]})).round(4)
+
+resultado
+# %%
+#Uma maneira de identificar a classe do resultado de acordo com o predict
+
+resultado.idxmax(axis=1)
+# %%
+# Adicionando as probabilidades de ocorrência de cada uma das
+#categorias de Y definidas pela modelagem, bem como a respectiva
+#classificação, ao dataframe original
+
+#Probabilidades de ocorrência das três categoriais
+#Definição do array 'phats':
+phats = modelo_atrasado.predict()
+phats
+# %%
+#Transformação do array 'phats' para o dataframe 'phats':
+phats = pd.DataFrame(phats)
+phats
+# %%
+#Concatenando o dataframe original com o dataframe 'phats':
+df_atrasado_multinomial = pd.concat([df_atrasado_multinomial, phats], axis=1)
+df_atrasado_multinomial
+# %%
+# Analisando o resultado de acordo com a categoria de resposta:
+classificacao = phats.idxmax(axis=1)
+classificacao
+# %%
+#Adicionando a categoria de resposta 'classificacao' ao dataframe original,
+#por meio da criação da variável 'predicao'
+df_atrasado_multinomial['predicao'] = classificacao
+df_atrasado_multinomial
+# %%
+#Criando a variável 'predicao_label' a partir da variável 'predicao',
+#respeitando os seguintes rótulos:
+# 0: não chegou atrasado
+# 1: chegou atrasado primeira aula
+# 2: chegou atrasado segunda aula
+
+df_atrasado_multinomial.loc[df_atrasado_multinomial['predicao']==0,
+                            'predicao_label'] ='não chegou atrasado'
+df_atrasado_multinomial.loc[df_atrasado_multinomial['predicao']==1,
+                            'predicao_label'] ='chegou atrasado primeira aula'
+df_atrasado_multinomial.loc[df_atrasado_multinomial['predicao']==2,
+                            'predicao_label'] ='chegou atrasado segunda aula'
+
+df_atrasado_multinomial
+# %%
+# Eficiência global do modelo
+
+#Criando uma tabela para comparar as ocorrências reais com as predições
+table = pd.pivot_table(df_atrasado_multinomial,
+                       index=['predicao_label'],
+                       columns=['atrasado'],
+                       aggfunc='size')
+
+#Substituindo 'nan' por zero
+table = table.fillna(0)
+table
+# %%
+#Transformando o dataframe 'table' para 'array', para que seja possível
+#estabelecer o atributo 'diagonal'
+table = table.to_numpy()
+table
+# %%
+#Eficiência global do modelo
+acuracia = table.diagonal().sum()/table.sum()
+acuracia
+# %%
+# Plotagens das probabilidades
+
+#Plotagem das smooth probability lines para a variável 'dist'
+
+# 0: não chegou atrasado
+# 1: chegou atrasado primeira aula
+# 2: chegou atrasado segunda aula
+
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.regplot(df_atrasado_multinomial['dist'],
+            df_atrasado_multinomial[0],
+            ci=False, ax=ax,
+            label='não chegou atrasado', order=4, marker=".")
+sns.regplot(df_atrasado_multinomial['dist'],
+            df_atrasado_multinomial[1],
+            ci=False, ax=ax,
+            label='chegou atrasado primeira aula', order=4, marker=".")
+sns.regplot(df_atrasado_multinomial['dist'],
+            df_atrasado_multinomial[2],
+            ci=False, ax=ax,
+            label='chegou atrasado segunda aula', order=4, marker=".")
+ax.set(ylabel='Probabilidades', xlabel='Distância percorrida')
+ax.legend()
+plt.show()
+# %%
+# Plotagens das probabilidades
+
+#Plotagem das smooth probability lines para a variável 'sem'
+
+# 0: não chegou atrasado
+# 1: chegou atrasado primeira aula
+# 2: chegou atrasado segunda aula
+
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.regplot(df_atrasado_multinomial['sem'],
+            df_atrasado_multinomial[0],
+            ci=False, ax=ax,
+            label='não chegou atrasado', order=4, marker=".")
+sns.regplot(df_atrasado_multinomial['sem'],
+            df_atrasado_multinomial[1],
+            ci=False, ax=ax,
+            label='chegou atrasado primeira aula', order=4, marker=".")
+sns.regplot(df_atrasado_multinomial['sem'],
+            df_atrasado_multinomial[2],
+            ci=False, ax=ax,
+            label='chegou atrasado segunda aula', order=4, marker=".")
+ax.set(ylabel='Probabilidades', xlabel='Semáforos no percurso')
+ax.legend()
+plt.show()
+# %%
+# Plotagem tridimensional para cada probabilidade de ocorrência de cada
+#categoria da variável dependente
+
+#Probabilidades de não chegar atrasado (função 'go' do pacote 'plotly')
+
+import plotly.io as pio
+pio.renderers.default = 'browser'
+
+trace = go.Mesh3d(
+    x=df_atrasado_multinomial['dist'], 
+    y=df_atrasado_multinomial['sem'],
+    z=df_atrasado_multinomial[0],
+    opacity=1, intensity=df_atrasado_multinomial[0], colorscale="Viridis")
+
+layout = go.Layout(
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
+    width=800,
+    height=800
+)
+
+data = [trace]
+
+plot_figure = go.Figure(data=data, layout=layout)
+
+plot_figure.update_layout(scene = dict(
+                        xaxis_title='dist',
+                        yaxis_title='sem',
+                        zaxis_title='não chegou atrasado'))
+
+plot_figure.show()
+# %%
+# Plotagem tridimensional para cada probabilidade de ocorrência de cada
+#categoria da variável dependente
+
+#Probabilidades de chegar atrasado à primeira aula (função 'go' do pacote
+#'plotly')
+
+pio.renderers.default = 'browser'
+
+trace = go.Mesh3d(
+    x=df_atrasado_multinomial['dist'], 
+    y=df_atrasado_multinomial['sem'],
+    z=df_atrasado_multinomial[1],
+    opacity=1, intensity=df_atrasado_multinomial[1], colorscale="Viridis")
+
+layout = go.Layout(
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
+    width=800,
+    height=800
+)
+
+data = [trace]
+
+plot_figure = go.Figure(data=data, layout=layout)
+
+plot_figure.update_layout(scene = dict(
+                        xaxis_title='dist',
+                        yaxis_title='sem',
+                        zaxis_title='chegou atrasado à primeira aula'))
+
+plot_figure.show()
+# %%
+# Plotagem tridimensional para cada probabilidade de ocorrência de cada
+#categoria da variável dependente
+
+#Probabilidades de chegar atrasado à segunda aula (função 'go' do pacote
+#'plotly')
+
+pio.renderers.default = 'browser'
+
+trace = go.Mesh3d(
+    x=df_atrasado_multinomial['dist'], 
+    y=df_atrasado_multinomial['sem'],
+    z=df_atrasado_multinomial[2],
+    opacity=1, intensity=df_atrasado_multinomial[2], colorscale="Viridis")
+
+layout = go.Layout(
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
+    width=800,
+    height=800
+)
+
+data = [trace]
+
+plot_figure = go.Figure(data=data, layout=layout)
+
+plot_figure.update_layout(scene = dict(
+                        xaxis_title='dist',
+                        yaxis_title='sem',
+                        zaxis_title='chegou atrasado à segunda aula'))
+
+plot_figure.show()
+# %%
+# Visualização das sigmóides tridimensionais em um único gráfico
+
+pio.renderers.default = 'browser'
+
+trace = go.Mesh3d(
+    x=df_atrasado_multinomial['dist'], 
+    y=df_atrasado_multinomial['sem'],
+    z=df_atrasado_multinomial[0],
+    opacity=1)
+
+layout = go.Layout(
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
+    width=800,
+    height=800
+)
+
+data = [trace]
+
+plot_figure = go.Figure(data=data, layout=layout)
+
+trace_1 = go.Mesh3d(
+            x=df_atrasado_multinomial['dist'], 
+            y=df_atrasado_multinomial['sem'],
+            z=df_atrasado_multinomial[1],
+            opacity=1)
+
+plot_figure.add_trace(trace_1)
+
+trace_2 = go.Mesh3d(
+            x=df_atrasado_multinomial['dist'], 
+            y=df_atrasado_multinomial['sem'],
+            z=df_atrasado_multinomial[2],
+            opacity=1)
+
+
+plot_figure.add_trace(trace_2)
+
+plot_figure.update_layout(scene = dict(
+                        xaxis_title='dist',
+                        yaxis_title='sem',
+                        zaxis_title='probabilidades'))
+
+plot_figure.show()
+################################## FIM ######################################
